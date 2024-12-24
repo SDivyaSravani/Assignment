d52db9902f001e2969d2cc328a2880a0f6925863
Divya <sankarasettydivyasravani@gmail.com>
2024-12-25 00:13:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
   useEffect(()=>{
     axios.get("https://randomuser.me/api/?page=1&results=1&seed=abc")
     .then((res)=>{
-      console.log(res.data.results[0].name.first);
       setDetails(res.data.results[0]);
     })
   },[])
@@ -19,7 +18,7 @@ const App = () => {
       <div className='bg-[#171d32] flex flex-wrap text-white font-bold text-2xl p-16 rounded-xl shadow-lg shadow-blue-950/50'>
       
       <div className='col-5 me-4'>
-              <img className='rounded-2xl' src={details.picture.large} alt="pic" />
+                      <img className='rounded-2xl' src={details.picture.large} alt="pic" />
               </div>
               <div className='col-7 ms-4'>
                   <p className='font-sans mb-2'>Name : {details.name.first}  {details.name.last}</p>
